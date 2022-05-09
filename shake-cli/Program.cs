aae0709193106ffb2012ac09b9f0c7dc8548f1de
Christopher Greaves <28542943+Chris-Greaves@users.noreply.github.com>
2022-05-10 00:10:15 +0100

    Initial Commit

---
+++ shake-cli/Program.cs
@@ -0,0 +1,36 @@
+﻿using shake.core;
+using System.CommandLine;
+
+var deviceOpt = new Option<string>("--device", getDefaultValue: () => "default", description: "The audio device to keep awake");
+var delayOpt = new Option<int>("--delay", getDefaultValue: () => 5, description: "The delay between audio cues");
+
+var listAudioCmd = new Command("list", description: "List audio devices to keep awake");
+
+listAudioCmd.SetHandler(() =>
+{
+    foreach (var device in AudioUtils.GetAudioDevices())
+    {
+        Console.WriteLine(device);
+    }
+});
+
+var audioCmd = new Command("audio", description: "Keep and audio device Awake by playing inaudible sounds on repeat")
+{
+    deviceOpt,
+    delayOpt,
+    listAudioCmd
+};
+
+audioCmd.SetHandler((string device, int delay) =>
+{
+    Console.WriteLine("Set Audio Device to keep awake called");
+    Console.WriteLine($"Selected Device: {device}");
+    Console.WriteLine($"Selected Delay: {delay}");
+}, deviceOpt, delayOpt);
+
+var root = new RootCommand
+{
+    audioCmd
+};
+
+return root.Invoke(args);
---
+++ shake.core/AudioUtils.cs
@@ -0,0 +1,16 @@
+﻿using NAudio.Wave;
+
+namespace shake.core
+{
+    public static class AudioUtils
+    {
+        public static IEnumerable<string> GetAudioDevices()
+        {
+            for (int n = -1; n < WaveOut.DeviceCount; n++)
+            {
+                var caps = WaveOut.GetCapabilities(n);
+                yield return $"{n}: {caps.ProductName}";
+            }
+        }
+    }
+}
